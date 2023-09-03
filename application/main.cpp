16667d567ba1903b9886d058fe7bbaab77bcb918
Cherkashyn Denys <d.a.cherkashin@gmail.com>
2023-09-03 10:38:56 +0300

    Drivers working version v.1.0

---
+++ application/main.cpp
@@ -0,0 +1,83 @@
+/**
+ ******************************************************************************
+ * @file           : main.cpp
+ * @author         :
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ *
+ *
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+#include "stm32f1xx.h"
+#include "i2c.h"
+
+
+// Разрешить исключения.
+//void __enable_irq(void);
+
+// Запретить все исключения.
+//void __disable_irq(void);
+
+// Разрешить прерывание с номером IRQn,
+// IRQn>=0 - только для внешних прерываний.
+//void NVIC_EnableIRQ(IRQn_Type IRQn);
+
+// Запретить прерывание с номером IRQn,
+// IRQn>=0 - только для внешних прерываний.
+//void NVIC_DisableIRQ(IRQn_Type IRQn);
+
+//I2C2_ER_IRQHandler
+
+std::unordered_map<uint8_t, uint8_t*> i2c_buff_it = {};
+uint8_t r_data[4];
+//uint8_t i2c_buff_pointer = 0;
+I2C* i2c;
+bool isFirstSkiped = false;
+
+extern "C" void I2C2_EV_IRQHandler(void)
+{
+
+   // i2c_buff_pointer = (*(&i2c_buff_it + 1) - i2c_buff_it)-1;
+    //i2c->slaveReadWrite(&i2c_buff_it, 1, true);
+    //i2c->slaveWrite(&i2c_buff_it, 1, true);
+   // i2c->slaveRead(r_data, 1, true);
+   // i2c->slaveWrite(&r_data[2], 1, true);
+    i2c->slaveReadWrite(r_data,1,true);
+   // i2c_buff_pointer = (i2c_buff_pointer>0) ? (i2c_buff_pointer-1) : (*(&i2c_buff_it + 1) - i2c_buff_it);
+}
+
+int main(void)
+{
+
+	//uint8_t r_data[4];
+	uint8_t s_data[4]= {0b00010001, 0b00100010, 0b00110011, 0b01000100};
+
+	i2c_buff_it[1] = r_data;
+	i2c_buff_it[2] = &r_data[1];
+	i2c_buff_it[3] = &r_data[2];
+	i2c_buff_it[4] = &r_data[3];
+	r_data[2] = 0b01110111;
+      //  uint8_t k=*(i2c_buff_it[3]);
+	i2c = I2C::getInstance(I2C2);
+	NVIC_EnableIRQ(I2C2_EV_IRQn);
+	__enable_irq();
+	
+	I2C* i2c1 = I2C::getInstance(I2C1);
+	i2c1->masterWrite(0x02, s_data, 2);
+	i2c1->masterRead(0x02, s_data, 3);
+	//i2c1->masterRead(0b0100111, 0, s_data, 3);
+	//i2c1->masterWrite(0x02, 2, s_data, 3);
+//	++i2c_buff_it;
+	//i2c1->masterRead(0x02, 8, s_data, 3);
+	/* Loop forever */
+	/*
+	while(1){
+
+	}
+*/
+	return 0;
+}
---
+++ drivers/inc/i2c.h
@@ -0,0 +1,96 @@
+#ifndef _I2C_H_
+	#define _I2C_H_
+
+	#include<unordered_map>
+
+	#ifdef __cplusplus
+		extern "C" {
+	#endif
+
+	#include <stdint.h>
+
+	//using namespace std;
+
+	//define STM device type
+	#include "stm32f1xx.h"
+
+	#define SM 	0
+	#define FM	1
+
+	#include "i2c_conf.h"
+	#include "i2c_def.h"
+
+	//=============================
+
+
+
+
+	#ifdef __cplusplus
+		}
+	#endif
+
+	 // --- CLASS DEFINITION ---//
+
+
+
+	class I2C final {
+
+		private:
+			typedef std::unordered_map<I2C_TypeDef*, I2C*> ConnectionMap;
+			static ConnectionMap connections;
+			I2C_TypeDef * i2cType;
+			uint32_t CR1LastState;
+			bool isSlaveActive;
+			bool isSlaveWrongRegAddr;
+			uint8_t slaveBuffCounter;
+			uint8_t *slaveBuffPointer;
+			uint8_t  slaveLastRegAddr;
+
+			I2C(I2C_TypeDef *, I2C_Cfg_TypeDef*);
+			~I2C();
+			I2C(const I2C&);
+			const I2C& operator = (const I2C&);
+
+			void gpioInit(I2C_TypeDef *);
+			void i2cInit(I2C_TypeDef *, I2C_Cfg_TypeDef*);
+
+			void start();
+			void stop();
+			bool sendAddress(uint16_t, uint16_t, bool);
+			bool pushDataByte(uint8_t);
+			bool pullDataByte(uint8_t*);
+			void delay();
+			bool isAddressBitSet();
+			void setMasterConnection(uint16_t, uint16_t, bool);
+			bool isSetSlaveConnection(uint8_t*);
+			bool isSetSlaveConnection(std::unordered_map<uint8_t, uint8_t*>*);
+
+
+		public:
+			static I2C* getInstance(I2C_TypeDef *, I2C_Cfg_TypeDef*);
+			static I2C* getInstance(I2C_TypeDef *);
+
+			void enable();
+			void disable();
+			bool isEnabled();
+			bool isFailures();
+			bool isStoped();
+			bool isMasterWrite();
+			bool isMasterRead();
+			bool isSlaveWrite();
+			bool isSlaveRead();
+
+			void masterWrite(uint16_t, uint16_t, uint8_t*, uint8_t);
+			void masterWrite(uint16_t, uint8_t*, uint8_t);
+			void masterRead(uint16_t, uint16_t, uint8_t*, uint8_t);
+			void masterRead(uint16_t, uint8_t*, uint8_t);
+
+			void slaveWrite(uint8_t*, uint8_t, bool);
+			void slaveWrite(uint8_t*, bool);
+			void slaveRead(uint8_t*, uint8_t, bool);
+			void slaveRead(uint8_t*, bool);
+			void slaveReadWrite(uint8_t*, uint8_t, bool);
+			void slaveReadWrite(std::unordered_map<uint8_t, uint8_t*>*,  uint8_t, bool);
+	};
+
+#endif /* _I2C_H_ */
